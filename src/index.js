17b002069c145cca07a9b858ab02a0a4fe6134c7
Byron McMullen <byronsm@gmail.com>
2018-12-12 11:44:39 -0800

    dev server setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 // TODO: The initial goal is just to connect 2 clients with a simple chat
+const registrationForm = document.getElementById("registration");
+
+function registerUser(event) {
+  event.preventDefault();
+  const username = document.getElementById("username").value;
+  console.log(username);
+}
+registrationForm.onsubmit = registerUser;
