c39f30dd4bc1def8993ab5e123ed6340d85aaaef
Jake Barnett <jake@apptentive.com>
2018-12-13 13:31:37 -0800

    Add webRTC connection logic.

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import websocket from "./websocket";
 
 class Game extends React.Component {
@@ -6,7 +7,27 @@ class Game extends React.Component {
   ws = websocket(this.props.username);
   // TODO: make this require a more conforming GUID
   gameName = location.pathname.split("/")[1];
+
+  peerConnection = new RTCPeerConnection();
+
+  createOffer = () => {
+    return this.peerConnection.createOffer()
+  };
+
+  sendOffer = (body) => {
+    axios.post(`/.netlify/functions/sendOffer?channel=${this.state.channel.name}`, body)
+  };
+
+  sendAnswer = (body) => {
+    axios.post(`/.netlify/functions/sendAnswer?channel=${this.state.channel.name}`, body)
+  };
+
   componentDidMount() {
+    const sendChannel = this.peerConnection.createDataChannel("sendChannel");
+    sendChannel.onopen = (e) => console.log("A ON OPEN", e);
+    sendChannel.onclose = (e) => console.log("A ON CLOSE", e);
+    sendChannel.onmessage = (e) => console.log("A, RECEIVED MESSAGE:", e.data);
+
     const channel = this.ws.subscribe(`presence-${this.gameName}-scrabble`);
     channel.bind("message", function(data) {
       alert(JSON.stringify(data));
@@ -14,8 +35,48 @@ class Game extends React.Component {
     channel.bind("pusher:subscription_succeeded", () => {
       const me = channel.members.me;
       let players = [];
-      channel.members.each(member => players.push(member));
-      this.setState({ me, players });
+      this.createOffer()
+        .then((offer) => {
+          channel.members.each(member => {
+            players.push(member);
+
+            if (me.id === member.id) return;
+
+            const body = {
+              offerMaker: me.id,
+              offerRecipient: member.id,
+              offer
+            };
+
+            this.peerConnection.setLocalDescription(offer);
+            this.sendOffer(JSON.stringify(body));
+          });
+
+          this.setState({ me, players });
+      });
+    });
+    channel.bind("rtcOffer", (data) => {
+      console.log(`offer: ${JSON.stringify(data, null, 2)}`);
+      if (data.offerRecipient === this.state.me.id) {
+        console.log(`${this.state.me.id} received offer for ${data.offerRecipient}`);
+        this.peerConnection.setRemoteDescription(data.offer)
+          .then(() => this.peerConnection.createAnswer())
+          .then((answer) => {
+            this.sendAnswer({
+              answerMaker: this.state.me.id,
+              answerRecipient: data.offerMaker,
+              answer
+            });
+            this.peerConnection.setLocalDescription(answer)
+          })
+      }
+    });
+    channel.bind('rtcAnswer', (data) => {
+      console.log(`answer: ${JSON.stringify(data, null, 2)}`)
+      if (data.answerRecipient === this.state.me.id) {
+        console.log(`${this.state.me.id} received answer for ${data.answerRecipient}`);
+        this.peerConnection.setRemoteDescription(data.answer)
+      }
     });
     channel.bind("pusher:member_added", () => {
       let players = [];
